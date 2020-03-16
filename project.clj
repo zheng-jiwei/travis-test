a522854cc036946deedb8cd41bd61402bb74251d
zheng-jiwei <zheng.jiwei@connecty.co.jp>
2020-03-16 11:23:53 +0900

    modify structur

--- project.clj
+++ project.clj
@@ -15,5 +15,5 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}}
-  :main ecsite.test
+  :main test.clojure.travis-test
 )
