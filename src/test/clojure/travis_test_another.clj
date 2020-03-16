fe0483e8b288bea16eaa9ecaadaf2c94e633f7f8
zheng-jiwei <zheng.jiwei@connecty.co.jp>
2020-03-16 19:49:02 +0900

    test

---
+++ src/test/clojure/travis_test_another.clj
@@ -0,0 +1,11 @@
+(ns test.clojure.travis-test-another
+    (:require
+      [ecsite.storefront :as sf]
+	  [clojure.test :refer :all]
+      )
+    )
+
+(deftest ^:zheng001 test-1
+	(prn "### travis-test-another test1")
+)
+
