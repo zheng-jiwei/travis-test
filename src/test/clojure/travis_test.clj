e6fbaa8ec9f5ff503c33b6e34b9a3443d2f7473f
zheng-jiwei <zheng.jiwei@connecty.co.jp>
2020-03-18 08:15:52 +0900

    delete comment of do-report

--- src/test/clojure/travis_test.clj
+++ src/test/clojure/travis_test.clj
@@ -53,14 +53,7 @@
 	)
 )
 
-;do-report を使って、テスト項目の中に１つ失敗なので、結果に falseがあって、1 failures, test failed
-;expected: (= "zheng.jiwei@yahoo.co.jp" (get result "email"))
-;  actual: (not (= "zheng.jiwei@yahoo.co.jp" "zheng.jiwei+001@connecty.co.jp"))
-;false
-;"##" nil
-;Ran 1 tests containing 2 assertions.
-;1 failures, 0 errors.
-;Tests failed.
+;do-reportのテストですが、exceptionが発生しまいた。
 (deftest ^{:test001 true} test-4
 	(let [result (sf/parse-token-info "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiaWdjb21tZXJjZSIsImV4cCI6MTU4NjM0NjUyMiwiY3VzdG9tZXJfaWQiOiIxIiwiZW1haWwiOiJ6aGVuZy5qaXdlaSswMDFAY29ubmVjdHkuY28uanAifQ.2ScGqKczUxKIFgnRW7_z_k-jn2aREMe7Lnaix0EX9tU" 1)]
       (when (is (= "1" (get result "customer_id")))
